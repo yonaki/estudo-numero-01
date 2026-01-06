ae695c80e60310923f8214fb6e1c3a1e9115804f
yonaki <viktor.nacao@gmail.com>
2026-01-06 08:07:48 +0000

    adicona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home(){
+    return (<div>
+        <h1>Teste numero 1 [teste de ip publico]</h1>
+        <img src="https://i.pinimg.com/736x/47/65/3b/47653bc58e32b20124ff27a020d10512.jpg" alt="Logo" />
+</div>
+    );
+}
+
+
+export default Home;
+
